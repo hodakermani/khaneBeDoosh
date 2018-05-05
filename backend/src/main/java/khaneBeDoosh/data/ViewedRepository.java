3a044fb7683a0cb5fc4ed4de919b74b68a71fc50
ns.sajadi <hwe.sajadi@yahoo.com>
2018-05-05 17:53:03 +0430

    refactor

--- backend/src/main/java/khaneBeDoosh/data/ViewedRepository.java
+++ backend/src/main/java/khaneBeDoosh/data/ViewedRepository.java
@@ -1,13 +1,10 @@
 package khaneBeDoosh.data;
 
-import khaneBeDoosh.domain.Individual;
-import khaneBeDoosh.domain.User;
 import khaneBeDoosh.domain.Viewed;
 
 import org.json.JSONException;
 import org.apache.log4j.Logger;
 
-import javax.jws.soap.SOAPBinding;
 import java.sql.*;
 import java.util.List;
 import java.util.ArrayList;
